98783559c4f243a317009acf6a4c6a497f133fcc
barbarabischain <bbischain@gmail.com>
2024-10-15 12:48:33 -0300

    norminette token_type

--- source/token_type.c
+++ source/token_type.c
@@ -6,7 +6,19 @@
 /*   By: babischa <babischa@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/16 15:18:40 by babischa          #+#    #+#             */
-/*   Updated: 2024/10/14 17:07:34 by babischa         ###   ########.fr       */
+/*   Updated: 2024/10/15 12:35:03 by babischa         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   token_type.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: babischa <babischa@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/09/16 15:18:40 by babischa          #+#    #+#             */
+/*   Updated: 2024/10/15 12:32:14 by babischa         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,20 +28,20 @@ int	token_type(char *str)
 {
 	if (!strcmp(str, "|"))
 		return (1);
-	else if ((!strcmp(str, "<")) || (!strcmp(str, ">")) ||
-		(!strcmp(str, "<<")) || (!strcmp(str, ">>")))
-        return (2);
-    return (0);
+	else if ((!strcmp(str, "<")) || (!strcmp(str, ">"))
+		|| (!strcmp(str, "<<")) || (!strcmp(str, ">>")))
+		return (2);
+	return (0);
 }
 
-void    print_type(char **str)
+void	print_type(char **str)
 {
-    int type;
+	int	type;
 
-    while (*str)
-    {
-        type = token_type(*str);
-        printf("command: %s -- type: %d\n", *str, type);
-        str++;
-    }
+	while (*str)
+	{
+		type = token_type(*str);
+		printf("command: %s -- type: %d\n", *str, type);
+		str++;
+	}
 }
