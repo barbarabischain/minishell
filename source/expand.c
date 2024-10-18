bf65993de5d8cbec7fb6fbf0110f5188b5ed7996
Marcelo Dias Machado <madias-m@c3r3p2.42sp.org.br>
2024-10-18 13:51:12 -0300

    norminette no expand

--- source/expand.c
+++ source/expand.c
@@ -6,7 +6,7 @@
 /*   By: madias-m <madias-m@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/02 15:39:21 by madias-m          #+#    #+#             */
-/*   Updated: 2024/10/17 18:23:23 by madias-m         ###   ########.fr       */
+/*   Updated: 2024/10/18 13:50:42 by madias-m         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,36 +22,53 @@ static int	var_end(char *var)
 	return (i);
 }
 
+static void	add_literal(t_node **dest, t_node *src, int *i)
+{
+	t_node	*node;
+
+	node = new_node(ft_substr(&src->value[*i], 0, 1));
+	(*i)++;
+	add_node_last(&*dest, node);
+}
+
+static void	add_expansion(t_node **dest, t_node *src, int *i)
+{
+	int			j;
+	char		*key;
+	t_env_list	*found;
+	t_node		*node;
+
+	(*i)++;
+	key = ft_substr(&src->value[*i], 0, var_end(&src->value[*i]));
+	*i = *i + var_end(&src->value[*i]);
+	found = lst_find(shell()->env_list, key);
+	if (found)
+	{
+		j = 0;
+		while (found->value[j])
+		{
+			node = new_node(ft_substr(&found->value[j++], 0, 1));
+			add_node_last(&*dest, node);
+		}
+	}
+	else
+		add_node_last(&*dest, new_node(ft_strdup(" ")));
+	free(key);
+}
+
 static void	expand_var(t_node	*token)
 {
 	t_node		*expand;
-	t_env_list	*found;
-	char		*key;
 	int			i;
-	int			j;
 
 	i = 0;
 	expand = NULL;
 	while (token->value[i])
 	{
 		if (token->value[i] != '$')
-			add_node_last(&expand, new_node(ft_substr(&token->value[i++], 0, 1)));
+			add_literal(&expand, token, &i);
 		else
-		{
-			i++;
-			key = ft_substr(&token->value[i], 0, var_end(&token->value[i]));
-			i += var_end(&token->value[i]);
-			found = lst_find(shell()->env_list, key);
-			if (found)
-			{
-				j = 0;
-				while (found->value[j])
-					add_node_last(&expand, new_node(ft_substr(&found->value[j++], 0, 1)));
-			}
-			else
-				add_node_last(&expand, new_node(ft_strdup(" ")));
-			free(key);
-		}
+			add_expansion(&expand, token, &i);
 	}
 	free(token->value);
 	token->value = nodes_to_string(expand);
