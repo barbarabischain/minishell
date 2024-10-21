d0ba7c569352673b2335ddfcf40860be06255ec1
mmachadociandt <mmachado@ciandt.com>
2024-10-21 12:33:52 -0300

    corrigido o problema com o heredoc

--- source/parser.c
+++ source/parser.c
@@ -6,7 +6,7 @@
 /*   By: madias-m <madias-m@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/30 16:37:00 by madias-m          #+#    #+#             */
-/*   Updated: 2024/10/17 18:12:35 by madias-m         ###   ########.fr       */
+/*   Updated: 2024/10/21 12:31:53 by madias-m         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,9 +14,11 @@
 
 static int	is_meta_character(char *actual)
 {
-	if (*actual == '|' || *actual == '<')
+	if (*actual == '|')
 		return (1);
 	else if (*actual == '>' && *(actual - 1) != '>')
+		return (1);
+	else if (*actual == '<' && *(actual - 1) != '<')
 		return (1);
 	return (0);
 }
@@ -34,8 +36,10 @@ static char	*put_space_on(char *str)
 			add_node_last(&temp, new_node(ft_strdup(" ")));
 		add_node_last(&temp, new_node(ft_substr(&str[i], 0, 1)));
 		if (is_meta_character(&str[i]) && ft_isalnum(str[i + 1]))
+		 	add_node_last(&temp, new_node(ft_strdup(" ")));
+		if (str[i] == '>' && str[i - 1] == '>' && ft_isalnum(str[i + 1]))
 			add_node_last(&temp, new_node(ft_strdup(" ")));
-		else if (str[i] == '>' && str[i - 1] == '>' && ft_isalnum(str[i + 1]))
+		if (str[i] == '<' && str[i - 1] == '<' && ft_isalnum(str[i + 1]))
 			add_node_last(&temp, new_node(ft_strdup(" ")));
 		i++;
 	}
