f57982ca138e9ea2bc0e4ce4b197321b27c3b179
barbarabischain <bbischain@gmail.com>
2024-11-06 19:37:11 -0300

    cd praticamente pronto

--- source/env.c
+++ source/env.c
@@ -1,39 +1,18 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   echo.c                                             :+:      :+:    :+:   */
+/*   env.c                                              :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: babischa <babischa@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/10/16 13:50:37 by babischa          #+#    #+#             */
-/*   Updated: 2024/11/06 19:31:08 by babischa         ###   ########.fr       */
+/*   Created: 2024/11/05 16:09:25 by babischa          #+#    #+#             */
+/*   Updated: 2024/11/06 11:28:17 by babischa         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "../include/minishell.h"
+//#include "../include/minishell.h"
 
+// env()
+// {
 
-void	echo(char **cmd_matrix)
-{
-	int	i;
-	int	n;
-
-	i = 1;
-	n = 42;
-	if (cmd_matrix[i] && !ft_strncmp(cmd_matrix[i], "-n", 3))
-	{
-		i++;
-		n = -1;
-	}
-	while (cmd_matrix[i])
-	{
-		printf("%s", cmd_matrix[i++]);
-		if (i != matrix_len(cmd_matrix))
-			printf(" ");
-	}
-	free_matrix(cmd_matrix);
-	if (n != 42)
-		return ;
-	else
-		printf ("\n");
-}
+// }
