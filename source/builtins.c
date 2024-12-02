ce95d944a234c48bd0e323fefe82b2ec5ddf602c
barbarabischain <bbischain@gmail.com>
2024-12-02 13:25:34 -0300

    builtins usando ***cmd_array - norminette

--- source/builtins.c
+++ source/builtins.c
@@ -6,7 +6,7 @@
 /*   By: babischa <babischa@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/14 11:40:57 by babischa          #+#    #+#             */
-/*   Updated: 2024/12/02 12:29:35 by babischa         ###   ########.fr       */
+/*   Updated: 2024/12/02 13:22:18 by babischa         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,14 +30,13 @@ void	execute_builtins(char	**cmd_array)
 	if (!ft_strncmp(*cmd_array, "pwd", 4))
 		pwd();
 	if (!ft_strncmp(*cmd_array, "echo", 5))
-	 	echo(cmd_array);
+		echo(cmd_array);
 	if (!ft_strncmp(*cmd_array, "export", 5))
-	 	export(cmd_array);
+		export(cmd_array);
 	if (!ft_strncmp(*cmd_array, "unset", 6))
-	 	unset(cmd_array);
+		unset(cmd_array);
 	if (!ft_strncmp(*cmd_array, "cd", 3))
 		cd(cmd_array);
 	if (!ft_strncmp(*cmd_array, "env", 6))
 		env(cmd_array);
-
 }
